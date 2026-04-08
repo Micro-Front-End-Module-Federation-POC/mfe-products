8bd5a0a58da683d45b530bc77a20dc1671067bc2
SnehaS2000 <Snehasoundarrajan4610@gmail.com>
2026-04-08 18:30:46 +0530

    mfe-products

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SharedStateService } from '@mfe/shared-state';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('mfe-products');
+export class App implements OnInit {
+  protected readonly cartCount = signal(0);
+  constructor(private sharedState: SharedStateService) {}
+
+  ngOnInit(): void {
+    this.sharedState.state$.subscribe(value => {
+      this.cartCount.set(value?.cartCount || 0);
+    });
+  }
+
+  addToCart() {
+    this.sharedState.updateCart(this.cartCount() + 1);
+  }
+
 }
